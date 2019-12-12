59af3510b05ccf61e3ba151e2bd1b6585bad2890
huyen tran <t.huyen.hust@gmail.com>
2019-12-12 10:36:23 +0000

    cake bake

---
+++ app/Model/Book.php
@@ -0,0 +1,80 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Book Model
+ *
+ * @property Category $Category
+ * @property Comment $Comment
+ * @property Writer $Writer
+ */
+class Book extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'id';
+
+
+	// The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Category' => array(
+			'className' => 'Category',
+			'foreignKey' => 'category_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'Comment' => array(
+			'className' => 'Comment',
+			'foreignKey' => 'book_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+
+/**
+ * hasAndBelongsToMany associations
+ *
+ * @var array
+ */
+	public $hasAndBelongsToMany = array(
+		'Writer' => array(
+			'className' => 'Writer',
+			'joinTable' => 'books_writers',
+			'foreignKey' => 'book_id',
+			'associationForeignKey' => 'writer_id',
+			'unique' => 'keepExisting',
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'finderQuery' => '',
+		)
+	);
+
+}
